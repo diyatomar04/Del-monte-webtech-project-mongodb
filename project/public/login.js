acad351484f1921ce89bde3c1021edbcdc7c7b57
diyatomar04 <117175207+diyatomar04@users.noreply.github.com>
2024-04-28 23:08:24 +0530

    Add files via upload

---
+++ project/public/login.js
@@ -0,0 +1,11 @@
+function openloginpg() {
+   window.location.href = "login.html";
+}
+
+function opencreatepg() {
+   window.location.href = "signup_pg.html";
+}
+
+function openhomepg() {
+   window.location.href = "homepg.html";
+}
